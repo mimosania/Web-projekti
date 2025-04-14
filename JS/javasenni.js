1ff25f2d79440cb1c2c6130a55885a744e84be6d
mimosania <stuomiko24@students.oamk.fi>
2025-04-14 22:15:44 +0300

    added placeholder pictures

---
+++ JS/javasenni.js
@@ -0,0 +1,9 @@
+const gridContainer = document.querySelector(".grid-container");
+let kortit = [];
+let kortti1, kortti2;
+let lockBoard = false;
+let pisteet = 0;
+
+document.querySelector(".pisteet").textContent = pisteet;
+
+fetch()
