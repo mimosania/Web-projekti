03ede4bc74db0d7efb804ab3e0b8cf3a996c99e3
mimosania <stuomiko24@students.oamk.fi>
2025-04-16 00:01:15 +0300

    added matching

--- JS/javasenni.js
+++ JS/javasenni.js
@@ -2,12 +2,28 @@ const kortit = document.querySelectorAll(".kortti");
 
 let kaannetty = false;
 let eka, toka;
+
 function flipkortti() {
     this.classList.toggle('flip');
-    if (!kaannetty){
-        kaannetty=true;
-        kaannetty = this;
-        console.log({kaannetty, eka});
+    if (!kaannetty) {
+        kaannetty = true;
+        eka = this;
+    } else{
+        kaannetty = false;
+        toka = this;
+
+        if (eka.dataset.framework === 
+            toka.dataset.framework) {
+            eka.removeEventListener("click", flipkortti);
+            toka.removeEventListener("click", flipkortti);
+        } else {
+            setTimeout(()=> {
+            eka.classList.remove("flip");
+            toka.classList.remove("flip");
+            }, 1500);
+
+
+        }
     }
 }
 
