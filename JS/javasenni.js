33451fa1d814b58dcad845aa21a4ce32153ef3ac
mimosania <stuomiko24@students.oamk.fi>
2025-04-16 00:19:12 +0300

    made sure you can't press the same card twice or cards before they turn back around

--- JS/javasenni.js
+++ JS/javasenni.js
@@ -1,9 +1,13 @@
 const kortit = document.querySelectorAll(".kortti");
 
 let kaannetty = false;
+let lockBoard = false;
 let eka, toka;
 
 function flipkortti() {
+    if(lockBoard) return;
+    if (this === eka) return;
+
     this.classList.toggle('flip');
     if (!kaannetty) {
         kaannetty = true;
@@ -11,20 +15,45 @@ function flipkortti() {
     } else{
         kaannetty = false;
         toka = this;
+        
+        pari();
 
+
+    }
+    function pari(){
         if (eka.dataset.framework === 
             toka.dataset.framework) {
-            eka.removeEventListener("click", flipkortti);
-            toka.removeEventListener("click", flipkortti);
-        } else {
-            setTimeout(()=> {
-            eka.classList.remove("flip");
-            toka.classList.remove("flip");
-            }, 1500);
+                poispaalta();
 
+        } else {
+            takaisin();
 
         }
+
     }
+}
+
+function poispaalta(){
+    eka.removeEventListener("click", flipkortti);
+    toka.removeEventListener("click", flipkortti);
+    resetBoard();
+
+}
+
+function takaisin(){
+    lockBoard = true;
+    setTimeout(()=> {
+        eka.classList.remove("flip");
+        toka.classList.remove("flip");
+
+        lockBoard = false;
+        resetBoard();
+        }, 1500);
+}
+
+function resetBoard(){
+[kaannetty, lockBoard] = [false, false];
+[eka, toka] = [ null, null];
 }
 
 kortit.forEach(kortti => kortti.addEventListener('click', flipkortti));
