382cba8fad5e4ec5c5a69454609422cdeb90b723
mimosania <stuomiko24@students.oamk.fi>
2025-04-15 23:50:01 +0300

    tried adding data

--- JS/javasenni.js
+++ JS/javasenni.js
@@ -1,7 +1,14 @@
 const kortit = document.querySelectorAll(".kortti");
 
+let kaannetty = false;
+let eka, toka;
 function flipkortti() {
     this.classList.toggle('flip');
+    if (!kaannetty){
+        kaannetty=true;
+        kaannetty = this;
+        console.log({kaannetty, eka});
+    }
 }
 
 kortit.forEach(kortti => kortti.addEventListener('click', flipkortti));
