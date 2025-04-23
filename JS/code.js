3dddded8ea5a09046c123c25f713b99f01bb1365
marjoniemela <c3nima03@students.oamk.fi>
2025-04-23 14:04:08 +0300

    added span id to summary

--- JS/code.js
+++ JS/code.js
@@ -1,3 +1,5 @@
 let palapelipisteet = Number(sessionStorage.getItem('palapeli'));
+let Planeettavisailu = Number(sessionStorage.getItem('Planeettavisailu'));
 let pointElem = document.getElementById('points');
 pointElem.textContent = 'Palapelin pisteet: ' + palapelipisteet
+
