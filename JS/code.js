2e92fe2259eb5ab4ed0d1f1d7a0bcb7e63645044
marjoniemela <c3nima03@students.oamk.fi>
2025-04-24 09:59:44 +0300

    added code

--- JS/code.js
+++ JS/code.js
@@ -1,5 +1,8 @@
-let palapelipisteet = Number(sessionStorage.getItem('palapeli'));
+const visailu = document.querySelector('#visailu');
 let Planeettavisailu = Number(sessionStorage.getItem('Planeettavisailu'));
+visailu.innerHTML = Planeettavisailu;
+
+let palapelipisteet = Number(sessionStorage.getItem('palapeli'));
 
 
 let pointElem = document.getElementById('points');
