64ebb167384e4e03b100ae11126abcfce63ccf1b
eeskola22 <eeskola22@students.oamk.fi>
2025-04-24 12:34:29 +0300

    Added points for matematiikkapeli

--- JS/code.js
+++ JS/code.js
@@ -5,3 +5,7 @@ visailu.innerHTML = Planeettavisailu + "/8";
 const tahtikuvio = document.querySelector('#tahtikuvio');
 let palapeli = Number(sessionStorage.getItem('palapeli'));
 tahtikuvio.innerHTML = palapeli + "/3";
+
+const matematiikka = document.querySelector('#matematiikka');
+let matikkapeli = Number(sessionStorage.getItem('matikkapeli'));
+matematiikka.innerHTML = matikkapeli;
