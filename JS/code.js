d1d2d7c6c880b26606f140e8ee2b34d8a3dd6534
marjoniemela <c3nima03@students.oamk.fi>
2025-04-24 08:51:55 +0300

    deleted article text-size

--- JS/code.js
+++ JS/code.js
@@ -1,5 +1,6 @@
 let palapelipisteet = Number(sessionStorage.getItem('palapeli'));
 let Planeettavisailu = Number(sessionStorage.getItem('Planeettavisailu'));
-let pointElem = document.getElementById('points');
-pointElem.textContent = 'Palapelin pisteet: ' + palapelipisteet
 
+
+let pointElem = document.getElementById('points');
+pointElem.textContent = 'Palapelin pisteet: ' + palapelipisteet;
